6e49fc272b87d69eea0452521863aa8bab3448c0
LTX <126674060+ltx0101@users.noreply.github.com>
2025-03-15 16:54:19 +0200

    Update Nanocord.ps1

--- Nanocord.ps1
+++ Nanocord.ps1
@@ -1,4 +1,4 @@
-﻿if (-not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
+if (-not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
     Start-Process powershell -ArgumentList "-File `"$($MyInvocation.MyCommand.Path)`"" -Verb RunAs
     exit
 }
@@ -50,8 +50,7 @@ Write-Host "You selected $browserName."
 
 $folderPath = "$env:USERPROFILE\Documents\Nanocord"
 $batFilePath = "$folderPath\$browserName.bat"
-$iconPath = "$PSScriptRoot\logo.ico"  
-$iconDestination = "$folderPath\logo.ico"  
+$iconDestination = "$folderPath\logo.ico"
 
 if (-not (Test-Path $folderPath)) {
     New-Item -Path $folderPath -ItemType Directory
@@ -67,11 +66,14 @@ if (Test-Path $batFilePath) {
     exit
 }
 
-if (Test-Path $iconPath) {
-    Copy-Item -Path $iconPath -Destination $iconDestination
-    Write-Host "Icon file copied to $iconDestination."
+$iconUrl = "${githubRepoUrl}logo.ico"
+Write-Host "Downloading logo.ico from GitHub..."
+Invoke-WebRequest -Uri $iconUrl -OutFile $iconDestination
+
+if (Test-Path $iconDestination) {
+    Write-Host "Icon downloaded and saved to $iconDestination."
 } else {
-    Write-Host "Icon file 'logo.ico' not found. Using default icon."
+    Write-Host "Failed to download the icon. Using default icon."
     $iconDestination = "shell32.dll, 2"
 }
 
