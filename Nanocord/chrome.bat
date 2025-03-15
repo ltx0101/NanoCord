b9b96d1505ad2d02995da7b68a1aa00f9a7a5681
LTX <126674060+ltx0101@users.noreply.github.com>
2025-03-15 16:00:34 +0200

    Add files via upload

---
+++ Nanocord/chrome.bat
@@ -0,0 +1,1 @@
+powershell -NoProfile -ExecutionPolicy Bypass -Command "& {$process = Start-Process chrome -ArgumentList '--app=https://discord.com/app --disable-background-mode --process-per-site --disable-extensions --disable-sync --disable-gpu-vsync --enable-low-end-device-mode --disable-gpu' -PassThru; Start-Sleep -Seconds 2; $process.ProcessorAffinity = 12;}"
